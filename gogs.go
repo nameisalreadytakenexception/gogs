bc76240215ed0d3dcb57bde5933f4a6d56df05dc
nameisalreadytakenexception <42404906+nameisalreadytakenexception@users.noreply.github.com>
2019-09-04 18:37:44 +0300

    Update gogs.go

--- gogs.go
+++ gogs.go
@@ -1,5 +1,5 @@
 // +build go1.8
-
+//go
 // Copyright 2014 The Gogs Authors. All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
