ccc0986c7c808407b20c7481d68ad5edb482241f
nameisalreadytakenexception <42404906+nameisalreadytakenexception@users.noreply.github.com>
2019-09-23 14:25:01 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@ sudo mkdir log
 sudo mkdir -p lock/subsys/
 sudo cp scripts/init/centos/gogs /etc/rc.d/init.d/
 sudo useradd -r git
-sudo chown -R git ${APP_DEPLOY_DIR}
+sudo chown -R git /var/app/
 service gogs start
 service gogs enable #??
