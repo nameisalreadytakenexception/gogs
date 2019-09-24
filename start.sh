55a6f0b659cc3367287dbc4273aa343a090b36af
nameisalreadytakenexception <42404906+nameisalreadytakenexception@users.noreply.github.com>
2019-09-24 09:36:59 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,9 +4,10 @@ sudo mkdir -p /home/gogs/gogs-repositories
 sudo mkdir -p lock/subsys/
 sudo cp scripts/init/centos/gogs /etc/rc.d/init.d/
 sudo useradd git
-sudo usermod -aG wheel
+sudo usermod -aG wheel git
+sudo sed -i "s/# %wheel/ %wheel/g" /etc/sudoers
 sudo chown -R git /var/app/
-sudo chown -R git /var/app/current/custom/conf/app.ini
+#sudo chown -R git /var/app/current/custom/conf/app.ini
 sudo chown -R git /home/gogs/
 sudo service gogs start
 
