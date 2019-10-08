dd7a9aa2f34fa416a910781d9a68016ed68df21f
nameisalreadytakenexception <42404906+nameisalreadytakenexception@users.noreply.github.com>
2019-10-08 11:30:50 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,21 +4,21 @@ sudo mkdir -p /home/gogs/gogs-repositories
 sudo mkdir -p lock/subsys/
 sudo cp scripts/init/centos/gogs /etc/rc.d/init.d/
 sudo useradd git
-sudo usermod -aG wheel git
-sudo sed -i "s/# %wheel/ %wheel/g" /etc/sudoers
+# sudo usermod -aG wheel git
+# sudo sed -i "s/# %wheel/ %wheel/g" /etc/sudoers
 sudo chown -R git /var/app/
 sudo chown -R git /var/app/current/custom/conf/app.ini
 sudo chown -R git /home/gogs/
 
-sudo service nginx stop
-sudo sed -i "s+http://127.0.0.1:5000+http://127.0.0.1:3000+g" /etc/nginx/conf.d/elasticbeanstalk/00_application.conf
-sudo service nginx restart
+# sudo service nginx stop
+# sudo sed -i "s+http://127.0.0.1:5000+http://127.0.0.1:3000+g" /etc/nginx/conf.d/elasticbeanstalk/00_application.conf
+# sudo service nginx restart
 sudo service gogs start
 
 
-# sudo touch /home/gogs/starttt.sh
-# sudo echo "#!/bin/sh" > /home/gogs/starttt.sh
-# sudo echo "sudo service gogs start" >> /home/gogs/starttt.sh
-# sudo echo "sudo chown -R git /var/app/current/custom/conf/app.ini" >> /home/gogs/starttt.sh
-# sudo chmod +x /home/gogs/starttt.sh
-# (crontab -l 2>/dev/null; echo "* * * * * /home/gogs/starttt.sh") | crontab -
+sudo touch /home/gogs/starttt.sh
+sudo echo "#!/bin/sh" > /home/gogs/starttt.sh
+sudo echo "sudo service gogs start" >> /home/gogs/starttt.sh
+sudo echo "sudo chown -R git /var/app/current/custom/conf/app.ini" >> /home/gogs/starttt.sh
+sudo chmod +x /home/gogs/starttt.sh
+(crontab -l 2>/dev/null; echo "* * * * * /home/gogs/starttt.sh") | crontab -
