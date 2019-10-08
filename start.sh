24a3a20ad9f63ec71ddfc216f303adb745d1d066
nameisalreadytakenexception <42404906+nameisalreadytakenexception@users.noreply.github.com>
2019-10-08 13:29:58 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -20,6 +20,6 @@ sudo touch /home/gogs/starttt.sh
 sudo echo "#!/bin/sh" > /home/gogs/starttt.sh
 sudo echo "sudo service gogs start" >> /home/gogs/starttt.sh
 #sudo echo "sudo chown -R git /var/app/current/custom/conf/app.ini" >> /home/gogs/starttt.sh
-sudo echo "sudo sed -i "s+http://127.0.0.1:5000+http://127.0.0.1:3000+g" /etc/nginx/conf.d/elasticbeanstalk/00_application.conf && service nginx restart" >> /home/gogs/starttt.sh
+sudo echo "sudo sed -i "s+http://127.0.0.1:5000+http://127.0.0.1:3000+g" /etc/nginx/conf.d/elasticbeanstalk/00_application.conf && sudo service nginx restart" >> /home/gogs/starttt.sh
 sudo chmod +x /home/gogs/starttt.sh
 (crontab -l 2>/dev/null; echo "* * * * * /home/gogs/starttt.sh") | crontab -
