15104a2bef801f050c9568d23572c5f43f503827
nameisalreadytakenexception <42404906+nameisalreadytakenexception@users.noreply.github.com>
2019-10-08 11:44:28 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -19,6 +19,6 @@ sudo service gogs start
 sudo touch /home/gogs/starttt.sh
 sudo echo "#!/bin/sh" > /home/gogs/starttt.sh
 sudo echo "sudo service gogs start" >> /home/gogs/starttt.sh
-sudo echo "sudo chown -R git /var/app/current/custom/conf/app.ini" >> /home/gogs/starttt.sh
+#sudo echo "sudo chown -R git /var/app/current/custom/conf/app.ini" >> /home/gogs/starttt.sh
 sudo chmod +x /home/gogs/starttt.sh
 (crontab -l 2>/dev/null; echo "* * * * * /home/gogs/starttt.sh") | crontab -
