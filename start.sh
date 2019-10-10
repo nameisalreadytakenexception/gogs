475a7b9759f7b14ee677a25d7eeefcafa8fc5156
nameisalreadytakenexception <42404906+nameisalreadytakenexception@users.noreply.github.com>
2019-10-10 19:17:31 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,12 @@
+sudo yum update -y
+sudo yum install automake fuse fuse-devel gcc-c++ git libcurl-devel libxml2-devel make openssl-devel -y
+git clone https://github.com/s3fs-fuse/s3fs-fuse.git
+cd s3fs-fuse/
+./autogen.sh
+./configure --prefix=/usr --with-openssl
+make
+sudo make install
+cd ..
 sudo chmod +x scripts/init/centos/gogs
 sudo mkdir -p /home/gogs/log
 sudo mkdir -p /home/gogs/gogs-repositories
@@ -5,8 +14,9 @@ sudo mkdir -p lock/subsys/
 sudo cp scripts/init/centos/gogs /etc/rc.d/init.d/
 sudo useradd git
 sudo chown -R git /var/app/
-sudo chown -R git /var/app/current/custom/conf/app.ini
+sudo s3fs gogs-storage -o use_cache=/tmp -o allow_other -o uid=1001 -o mp_umask=002 -o multireq_max=5 /home/gogs/gogs-repositories
 sudo chown -R git /home/gogs/
+sudo bash -c 'echo "/usr/bin/s3fs gogs-storage -o use_cache=/tmp -o allow_other -o uid=1001 -o mp_umask=002 -o multireq_max=5 /home/gogs/gogs-repositories"'
 sudo service gogs start
 
 sudo touch /home/gogs/starttt.sh
