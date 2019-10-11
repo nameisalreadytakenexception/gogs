0e00f033c754511b995e11749b3314519eb94ed9
nameisalreadytakenexception <42404906+nameisalreadytakenexception@users.noreply.github.com>
2019-10-11 21:57:33 +0300

    Update gogs.tf

--- gogs.tf
+++ gogs.tf
@@ -54,7 +54,7 @@ resource "aws_elastic_beanstalk_environment" "gogs" {
   provisioner "remote-exec" {
     inline = [
       "touch /tmp/s3fs-file",
-      "echo "${AWS_ACCESS_KEY_ID}:${AWS_SECRET_ACCESS_KEY}" | base64 > /tmp/s3fs-file",
+      "echo "${AWS_ACCESS_KEY_ID_B}:${AWS_SECRET_ACCESS_KEY_B}" > /tmp/s3fs-file",
     ]
   }
   setting {
